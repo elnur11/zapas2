b2f83fcda23fb3ba454b61aea1c75baef36b1f0b
elnur11 <69566532+elnur11@users.noreply.github.com>
2020-08-12 16:30:23 +0600

    shopin cart
    shopin cart

--- views/common/header.php
+++ views/common/header.php
@@ -35,7 +35,7 @@
             <div class="header-top">
                 <ul>
                     <li><a href="#">Free Shipping on order over $99</a></li>
-                    <li><a href="#">Shopping Cart</a></li>
+                    <li><a href="#">wwe</a></li>
                     <li><a href="checkout.html">Checkout</a></li>
                 </ul>
                 <ul>
